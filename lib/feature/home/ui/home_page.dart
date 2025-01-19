1ceb1f413f3699df3b2791923ed3cf6709b2c56e
Raheel Khan <raheelk.dev@gmail.com>
2025-01-19 10:24:29 +0500

    products list refactored, cached network image added

--- lib/feature/home/ui/home_page.dart
+++ lib/feature/home/ui/home_page.dart
@@ -4,6 +4,7 @@ import 'package:products_store_bloc/feature/auth/ui/page/login_page.dart';
 import 'package:products_store_bloc/feature/home/bloc/product_bloc.dart';
 import 'package:products_store_bloc/feature/home/bloc/product_state.dart';
 import 'package:products_store_bloc/feature/home/model/product.dart';
+import 'package:products_store_bloc/feature/home/ui/widgets/product_item.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -39,92 +40,8 @@ class _HomePageState extends State<HomePage> {
             return ListView.builder(
               itemCount: state.products.length,
               itemBuilder: (context, index) {
-
                 final product = Product.fromJson(state.products[index]);
-
-                return Card(
-                  margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  elevation: 4,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Product Image
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.network(
-                            product.image,
-                            height: 80.0,
-                            width: 80.0,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        const SizedBox(width: 16.0),
-                        // Product Details
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              // Title
-                              Text(
-                                product.title,
-                                style: const TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              const SizedBox(height: 8.0),
-                              // Category
-                              Text(
-                                product.category,
-                                style: TextStyle(
-                                  fontSize: 14.0,
-                                  color: Colors.grey[600],
-                                ),
-                              ),
-                              const SizedBox(height: 8.0),
-                              // Price
-                              Text(
-                                '\$${product.price.toStringAsFixed(2)}',
-                                style: const TextStyle(
-                                  fontSize: 14.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.green,
-                                ),
-                              ),
-                              const SizedBox(height: 8.0),
-                              // Rating
-                              Row(
-                                children: [
-                                  const Icon(Icons.star, color: Colors.amber, size: 16.0),
-                                  const SizedBox(width: 4.0),
-                                  Text(
-                                    product.rating.rate.toString(),
-                                    style: const TextStyle(fontSize: 14.0),
-                                  ),
-                                  const SizedBox(width: 8.0),
-                                  Text(
-                                    '(${product.rating.count} reviews)',
-                                    style: TextStyle(
-                                      fontSize: 12.0,
-                                      color: Colors.grey[600],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
+                return ProductItem(product);
               },
             );
           }
---
+++ lib/feature/home/ui/widgets/product_item.dart
@@ -0,0 +1,103 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:products_store_bloc/feature/home/model/product.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem(
+    this.product, {
+    super.key,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Product Image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: CachedNetworkImage(
+                imageUrl: product.image,
+                placeholder: (context, url) =>
+                    const CupertinoActivityIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+                height: 80.0,
+                width: 80.0,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(width: 16.0),
+            // Product Details
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Title
+                  Text(
+                    product.title,
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8.0),
+                  // Category
+                  Text(
+                    product.category,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  // Price
+                  Text(
+                    '\$${product.price.toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0),
+                  // Rating
+                  Row(
+                    children: [
+                      const Icon(Icons.star, color: Colors.amber, size: 16.0),
+                      const SizedBox(width: 4.0),
+                      Text(
+                        product.rating.rate.toString(),
+                        style: const TextStyle(fontSize: 14.0),
+                      ),
+                      const SizedBox(width: 8.0),
+                      Text(
+                        '(${product.rating.count} reviews)',
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
